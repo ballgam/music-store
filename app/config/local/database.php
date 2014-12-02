8a6bb83164a9a3c2ac854ebf0308a5dfdaf86cd6
Anthony Waichungo <tondegwa@outlook.com>
2014-12-02 11:05:39 +0300

    connection in local db folder changed

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,10 +22,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'host'      => '104.236.15.227',
+			'database'  => 'music_store',
+			'username'  => 'root',
+			'password'  => 'PSV.Pass?1',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
